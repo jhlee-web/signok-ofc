3b8be72b5bf15ccc619737927f426f480ee948c6
kksuin <wlgus828@naver.com>
2023-02-20 00:16:11 +0900

    css 수정하면서 마크업 추가수정 필요. 메인 기본 마크업 완료

--- statics/js/app.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-    // header,footer load
-    $('.header').load('/template/include/inc_headerwrap.html');
-    $('.footer').load('//template/include/inc_footerwrap.html')  
-})
---
+++ statics/js/common.js
@@ -0,0 +1,5 @@
+$(function(){
+    // header,footer load
+    $('.header').load('/template/include/inc_headerwrap.html');
+    $('.footer').load('/template/include/inc_footerwrap.html')  
+})
