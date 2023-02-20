2efa7b5035774ae088174f195b52ca87e4057beb
kksuin <wlgus828@naver.com>
2023-02-20 16:38:18 +0900

    충돌일어날듯

---
+++ statics/js/main2.js
@@ -0,0 +1,6 @@
+$(function(){
+    for(let i = 1; i <=15; i++){
+        let item = $('<li class="logo__item"><img alt="로고'+i+'" src="/statics/img/ci/f_logo'+i+'_c.jpg"</li>')
+        $('#main-logo-section .logo-wrap').append(item)
+    }
+})
