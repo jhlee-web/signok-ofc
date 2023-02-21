a8bcccb92619cb8e20154abe8770be2724dfe55b
kksuin <wlgus828@naver.com>
2023-02-21 11:05:39 +0900

    modal 추가

--- statics/js/common.js
+++ statics/js/common.js
@@ -3,3 +3,14 @@ $(function(){
     $('.header').load('/template/include/inc_headerwrap.html');
     $('.footer').load('/template/include/inc_footerwrap.html')  
 })
+
+function openModal(pageName,modalName){
+    let modalBg = $('<div onclick="removeModal(this)"  class="modal__background"></div>')
+    let modal = $('<img class="modal" src="/modal/modal_'+pageName+'_'+modalName+'.png">')
+    modalBg.append(modal)
+    $('body').append(modalBg);
+}
+
+function removeModal(_this){
+    $(_this).remove();
+}
